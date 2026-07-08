16f1da9090a4dc06aa09edf06231423963bcb2c9
Amirhossein Hajilari <56062640+haajilari@users.noreply.github.com>
2026-07-08 13:24:19 +0330

    menu item - booking item - models added

--- LittleLemon/LittleLemon/urls.py
+++ LittleLemon/LittleLemon/urls.py
@@ -1,7 +1,13 @@
-from django.contrib import admin 
-from django.urls import path, include  
- 
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from Restaurant import views
+
+router = DefaultRouter()
+router.register(r"tables", views.BookingViewSet, basename="booking")
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('restaurant/', include('Restaurant.urls')),
+    path("admin/", admin.site.urls),
+    path("restaurant/", include("Restaurant.urls")),
+    path("restaurant/booking/", include(router.urls)),
 ]
--- LittleLemon/Restaurant/urls.py
+++ LittleLemon/Restaurant/urls.py
@@ -1,8 +1,8 @@
-from django.contrib import admin 
-from django.urls import path 
-from . import views 
-  
-urlpatterns = [ 
-    path('', views.index, name='index'),
-    path('home/', views.home, name='home')
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("menu/items/", views.MenuItemsView.as_view(), name="menu-items"),
+    path("menu/items/<int:pk>/", views.SingleMenuItemView.as_view(), name="single-menu-item"),
 ]
